275eb93b89bff579244b3d234268dd0a2603620d
HSH <335672336@qq.com>
2023-11-10 02:37:21 +0800

    ref computed

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -1,43 +1,26 @@
-// import { ReactiveEffect } from './effect'
-
-// class ComputedImpl {
-//   private _getter: Function
-//   private _dirty: boolean = true
-//   private _value: any
-//   private _effect: ReactiveEffect
-//   constructor(getter: Function) {
-//     this._getter = getter
-//     this._effect = new ReactiveEffect(getter, {
-//       scheduler: () => {
-//         if (!this._dirty) {
-//           this._dirty = true
-//         }
-//       }
-//     })
-//   }
-//   get value() {
-//     if (this._dirty) {
-//       this._dirty = false
-//       this._value = this._effect.run()  
-//     }
-//     return this._value
-//   }
-// }
-
-// export function computed(getter) {
-//   return new ComputedImpl(getter)
-// }
+import { ReactiveEffect } from './effect'
 
 class ComputedImpl {
   private _getter: Function
+  private _dirty: boolean = true
   private _value: any
-
-  constructor(getter) {
+  private _effect: ReactiveEffect
+  constructor(getter: Function) {
     this._getter = getter
+    this._effect = new ReactiveEffect(getter, {
+      scheduler: () => {
+        if (!this._dirty) {
+          this._dirty = true
+        }
+      }
+    })
   }
-
   get value() {
-    return this._value = this._getter()
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._effect.run()  
+    }
+    return this._value
   }
 }
 
