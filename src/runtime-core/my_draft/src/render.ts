9d14434c81e4452247f7beccda011da01c2dade2
HSH <335672336@qq.com>
2023-11-25 01:22:27 +0800

    shapeFlags 与位运算

--- src/runtime-core/my_draft/src/render.ts
+++ src/runtime-core/my_draft/src/render.ts
@@ -1,3 +1,4 @@
+import { ShapeFlags } from "../../../shared/shapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode, container) {
@@ -7,11 +8,12 @@ export function render(vnode, container) {
 
 // patch 处理虚拟节点
 function patch(vnode, container) {
-  if(typeof vnode.type === 'object') {
+  const {shapeFlag} = vnode
+  if(shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     // 处理组件 component
     
     processComponent(vnode, container)
-  } else if (typeof vnode.type === 'string') {
+  } else if (shapeFlag & ShapeFlags.ELEMENT) {
     // 处理元素 element
     processElement(vnode, container)
   }
@@ -25,11 +27,11 @@ function processElement(vnode, container) {
 
 function mountElement(vnode, container) {
   const el = (vnode.el = document.createElement(vnode.type))
-  const {children} = vnode
-  if(typeof children === 'string') {
+  const {children, shapeFlag} = vnode
+  if(shapeFlag & ShapeFlags.TEXT_CHILDREN) {
 
     el.textContent = children
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el)
   }
 
--- src/runtime-core/my_draft/src/vnode.ts
+++ src/runtime-core/my_draft/src/vnode.ts
@@ -1,11 +1,25 @@
-
+import { ShapeFlags } from '../../../shared/shapeFlags'
 
 export function createVNode(type, props = {}, children?) {
   const vnode = {
     type,
     props,
     children,
-    el: null 
+    shapeFlag: getShapeFlag(type),
+    el: null,
   }
+  // 判断 children 类型
+  if (typeof children === 'string') {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+  }
+
   return vnode
+}
+
+function getShapeFlag(type) {
+  return typeof type === 'string'
+    ? ShapeFlags.ELEMENT
+    : ShapeFlags.STATEFUL_COMPONENT
 }
